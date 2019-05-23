18b795d9ce4ed399c319bf744039a803edb1d9b5
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2019-05-23 19:46:34 +0100

    update

--- svhn.py
+++ svhn.py
@@ -1,16 +1,13 @@
 import tensorflow as tf
 import numpy as np
 from scipy.io import loadmat
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping, CSVLogger
-import keras
-import keras.backend as K
-import time
+from sklearn.metrics import f1_scoreimport time
 import os.path
 import subprocess
 
@@ -31,7 +28,7 @@ def f1_score(y_true, y_pred):
     return K.mean(f1)
 
 
-
+# loading data
 def loading_data():
 
     # First, download dataset (train, test and extra)
@@ -73,81 +70,10 @@ def loading_data():
     
     return X_train, X_test, X_val, y_train, y_test, y_val
 
-'''
-# View images
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(X_train[i], cmap=plt.cm.binary)
-    plt.xlabel(y_train[i])
-'''
+# define models
+def model():
 
-
-'''
     model = Sequential()
-    model.add(ZeroPadding2D((1,1),input_shape=(32,32,3)))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-    model.add(Flatten())
-    model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(2048, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(10, activation='softmax'))
-
-
-    model.summary()
-'''
-if __name__ == '__main__':
-    
-    # Loading train and test data
-    X_train, X_test, X_val, y_train, y_test, y_val = loading_data()
-    # Train : Test : Validation = 65931 : 26032 : 7326
-    print('Train data shape: ', X_train.shape)
-    print('Test data shape: ', X_test.shape)
-    print('Validation data shape: ', X_val.shape)
-    print('label_train shape: ', y_train.shape)
-    print('label_test shape: ', y_test.shape)
-    print('label_validation shape: ', y_val.shape)
-    
-    model = Sequential()
-
     model.add(Conv2D(32, (3, 3), input_shape=(32,32,3)))
     model.add(BatchNormalization(axis=-1))
     model.add(Activation('relu'))
@@ -165,19 +91,14 @@ if __name__ == '__main__':
     model.add(MaxPooling2D(pool_size=(2,2)))
 
     model.add(Flatten())
-
     # Fully connected layer
     model.add(Dense(512))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Dropout(0.4))
     model.add(Dense(10))
-
     model.add(Activation('softmax'))
-
     model.summary()
-
-
 
     # Helper: Save the model.
     checkpointer = ModelCheckpoint(
@@ -195,11 +116,24 @@ if __name__ == '__main__':
     timestamp = time.time()
     csv_logger = CSVLogger(os.path.join('./', 'logs' ,'training-' + str(timestamp) + '.log'))
 
-
     model.compile(optimizer='adam',
                  loss='sparse_categorical_crossentropy',
                  metrics=['accuracy'])
+    return model
 
+
+def traintest():
+    # Loading train and test data
+    X_train, X_test, X_val, y_train, y_test, y_val = loading_data()
+    # Train : Test : Validation = 65931 : 26032 : 7326
+    print('Train data shape: ', X_train.shape)
+    print('Test data shape: ', X_test.shape)
+    print('Validation data shape: ', X_val.shape)
+    print('label_train shape: ', y_train.shape)
+    print('label_test shape: ', y_test.shape)
+    print('label_validation shape: ', y_val.shape)
+
+    model = model()
     model.fit(X_train,
               y_train,
               epochs=20,
@@ -207,9 +141,14 @@ if __name__ == '__main__':
               validation_data=(X_val, y_val),
               callbacks=[tb, early_stopper, csv_logger, checkpointer],
               verbose=1)
-    # Evaluate
-    results = model.evaluate(X_test, y_test)
-    print(results)
+
+    # predict labels for testing set
+    y_predict = model.predict(X_test, batch_size=128)
+    average_f1 = f1_score(y_test, y_predict, average='macro')
+
+if __name__ == '__main__':
+    traintest()
+
 
 
 
